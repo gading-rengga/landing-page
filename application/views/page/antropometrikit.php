ffedee44b0bac5766c87935d4e7250709a857456
Gading Sobad <gading.rengga@gmail.com>
2022-10-03 16:03:18 +0700

    Update Tittle

--- application/views/page/antropometrikit.php
+++ application/views/page/antropometrikit.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Landing Page</title>
+	<title>Antropometrikit</title>
 	<link rel="shortcut icon" href="<?= base_url('assets') ?>/img/sobad-logo-circle.png">
 	<link rel="stylesheet" href="<?= base_url('assets') ?>/css/sobad-style.css">
 	<link rel="stylesheet" href="<?= base_url('assets') ?>/bootstrap-4/css/bootstrap.min.css">
