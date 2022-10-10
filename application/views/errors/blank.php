fd1bf17802ec04d7b904e9ab4c016820a0a9eb64
Gading Sobad <gading.rengga@gmail.com>
2022-10-10 14:33:46 +0700

    Update

---
+++ application/views/errors/blank.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blank-404</title>
+    <link rel="shortcut icon" href="<?= base_url('assets') ?>/img/sobad-logo-circle.png">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/css/sobad-style.css">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/bootstrap-4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugin/owl-carousel/css/owl.carousel.min.css">
+</head>
+
+<body>
+    <div class="page-wrap d-flex flex-row align-items-center" style="min-height: 100vh;">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">
+                        <h1>404</h1>
+                    </span>
+                    <div class="mt-4 lead">The page you are looking for was not found.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
